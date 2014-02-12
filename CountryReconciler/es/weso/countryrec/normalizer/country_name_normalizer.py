02b84380661ec0f0ae2890940c578ab5f4c3fef0
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2014-02-12 14:07:23 +0100

    Normalize country names in fr, es and en

--- CountryReconciler/es/weso/countryrec/normalizer/country_name_normalizer.py
+++ CountryReconciler/es/weso/countryrec/normalizer/country_name_normalizer.py
@@ -3,8 +3,13 @@ Created on 10/02/2014
 
 @author: Dani
 '''
-
+import re
 class CountryNameNormalizer(object):
+    
+    SUB_EXP_ES = "(el|las|los|la|lo|de|y|&|del|en)"
+    SUB_EXP_EN = "(the|in|of|and|&)"
+    SUB_EXP_FR = "(les|las|le|la|et|&|dans|de|d)"
+
     '''
     classdocs
     '''
@@ -16,13 +21,78 @@ class CountryNameNormalizer(object):
         '''
     @staticmethod
     def normalize_es_country(original):
-        return original
+        result = CountryNameNormalizer.general_normalization(original)
+        result = CountryNameNormalizer.rem_words_by_languaje(result, CountryNameNormalizer.SUB_EXP_ES)
+        return result
     
     @staticmethod
     def normalize_fr_country(original):
-        return original
+        result = CountryNameNormalizer.general_normalization(original)
+        result = CountryNameNormalizer.rem_words_by_languaje(result, CountryNameNormalizer.SUB_EXP_FR)
+        return result
     
     @staticmethod
     def normalize_en_country(original):
-        return original
+        result = CountryNameNormalizer.general_normalization(original)
+        result = CountryNameNormalizer.rem_words_by_languaje(result, CountryNameNormalizer.SUB_EXP_EN)
+        return result
+    
+    @staticmethod
+    def rem_words_by_languaje(original, sub_exp):
+#         
+#         regex_exp contains a list of non-significant words that should be replaced
+#         by a blank. To fit in the regex, each word should be in the middle of
+#         some of this pairs:
+#          - [white_space] word [white_space]
+#          - [white_space] word [end_of_string]
+#          - [start_of_the_string] word [end_of_string]
+#         
+        regex_exp = "(\A"+sub_exp+"\s)|(\s"+sub_exp+"\s)|(\s"+sub_exp+"\Z)"
+        
+        version1 = original
+        version2 = ""
+        while version1 != version2:
+            version1 = version2
+            version2 = re.sub( regex_exp, " ", version1)
+#         The previous loop, applying re.sub more than one time to the original chain
+#         should be done because if more than 1 unsignificant words come in a streak,
+#         some of them coul be ignored by the regex. E.g.: "Republic of the Congo". 
+#         " of " will fit in the regex, but that means that " the " won´t be recognized.
+#         The white space between "of" and "the" will be used only in one of the substrings,
+#         so in fact we hace " of " and "the ", and the resultant string will be 
+#         "Republic the Congo". If we apply more than one time the regex, this things 
+#         would be avoided
+        
+        return version2
+    
+    @staticmethod
+    def general_normalization(original):
+        result = original.lower()
+        result = CountryNameNormalizer.substitute_conflictive_chars(result)
+        result = result.strip()
+        
+        return result
+        
+    @staticmethod
+    def substitute_conflictive_chars(original):
+        result = original
+        #Vowel a
+        result = re.sub("à|á|â|ä", "a", result)
+        #Vowel e
+        result = re.sub("é|è|ê|ë", "e", result)       
+        #Vowel i
+        result = re.sub("í|ì|î|ï", "i", result)
+        #Vowel o
+        result = re.sub("ó|ò|ö|ô", "o", result)
+        #Vowel u
+        result = re.sub("ù|ú|û|ü", "u", result)
+        #Char ç
+        result = re.sub("ç", "c", result)
+        #char ñ
+        result = re.sub("ñ", "n", result)
+        #special chars
+        result = re.sub("¨|'|`|´|^|!|¡|?|¿", " ", result)
+        #----------return
+        return result
+        
         
--- CountryReconciler/es/weso/countryrec/test/MainTest.py
+++ CountryReconciler/es/weso/countryrec/test/MainTest.py
@@ -7,11 +7,25 @@ Created on 10/02/2014
 
 if __name__ == '__main__':
     
-    from es.weso.countryrec.country_reconciler import CountryReconciler
+    import re
+#     from es.weso.countryrec.country_reconciler import CountryReconciler
+#     from es.weso.countryrec.normalizer.country_name_normalizer import CountryNameNormalizer
     
-    rec = CountryReconciler()
-     
-#     config = ConfigParser()
-#     config.read("../../../../files/configuration.ini")
-    print len(rec.clist)
+#     rec = CountryReconciler()
+#      
+# #     config = ConfigParser()
+# #     config.read("../../../../files/configuration.ini")
+#     print len(rec.clist)
+    
+    sub_exp = "el|la|los|las|de|y|&"
+    chain = "el cacas de la el los el los y huevos y su lamina electrica del copon las"
+    
+    sub_exp = "(el|las|los|la|lo|de|y|&|del)"
+    s= chain
+    for i in range(1,2):
+        s = re.sub("(\A"+sub_exp+"\s)|(\s"+sub_exp+"\s)|(\s"+sub_exp+"\Z)" , " ", s)
+    print chain
+    print s
+    print s.strip() 
+
     
