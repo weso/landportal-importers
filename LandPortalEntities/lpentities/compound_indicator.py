df119ac39c91169676955d9a81ea45b25426ecd8
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2014-03-26 11:50:47 +0100

    Adapt indicator and compound_indicator to store translations

--- LandPortalEntities/lpentities/compound_indicator.py
+++ LandPortalEntities/lpentities/compound_indicator.py
@@ -5,9 +5,11 @@ from .indicator import Indicator
 
 class CompoundIndicator(Indicator):
 
-    def __init__(self, chain_for_id, int_for_id, name=None, description=None,
+    def __init__(self, chain_for_id, int_for_id, name_en=None, name_es=None,
+                 name_fr=None, description_en=None, description_es=None, description_fr=None,
                  dataset=None, measurement_unit=None, indicators=None):
-        super(CompoundIndicator, self).__init__(chain_for_id, int_for_id, name, description,
+        super(CompoundIndicator, self).__init__(chain_for_id, int_for_id, name_en, name_es,
+                                                name_fr, description_en, description_es, description_fr,
                                                 dataset, measurement_unit)
 
         self.indicators = indicators
--- LandPortalEntities/lpentities/indicator.py
+++ LandPortalEntities/lpentities/indicator.py
@@ -10,13 +10,18 @@ class Indicator(object):
     classdocs
     '''
 
-    def __init__(self, chain_for_id, int_for_id, name=None, description=None,
-                 dataset=None, measurement_unit=None):
+    def __init__(self, chain_for_id, int_for_id, name_en=None ,name_es=None,
+                 name_fr=None, description_en=None, description_es=None,
+                 description_fr=None, dataset=None, measurement_unit=None):
         '''
         Constructor
         '''
-        self.name = name
-        self.description = description
+        self.name_en = name_en
+        self.name_es = name_es
+        self.name_fr = name_fr
+        self.description_en = description_en
+        self.description_es = description_es
+        self.description_fr = description_fr
         self.dataset = dataset
         self.measurement_unit = measurement_unit
 
