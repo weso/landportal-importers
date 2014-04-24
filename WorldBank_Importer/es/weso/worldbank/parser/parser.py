fb3362f9e52b93ed27949a6f66db73ea71dd048e
Borja <kazeborja@gmail.com>
2014-04-24 11:00:17 +0200

    Refactor of WorldBank parser
    Make it look a bit more like the others adding configuration properties
    and separating functionality in different methods.
    
    Now it validated countries against the CountryReconciler

--- WorldBank_Importer/es/weso/worldbank/parser/parser.py
+++ WorldBank_Importer/es/weso/worldbank/parser/parser.py
@@ -3,28 +3,28 @@ Created on 18/12/2013
 
 @author: Nacho
 """
-import logging
 import ConfigParser
 from datetime import datetime
+import logging
 
-from lpentities.user import User
-from lpentities.organization import Organization
+from lpentities.computation import Computation
 from lpentities.data_source import DataSource
 from lpentities.dataset import Dataset
-from lpentities.license import License
-from lpentities.country import Country
-from lpentities.observation import Observation
-from lpentities.computation import Computation
-from lpentities.measurement_unit import MeasurementUnit
 from lpentities.indicator import Indicator
+from lpentities.instant import Instant
+from lpentities.license import License
+from lpentities.measurement_unit import MeasurementUnit
+from lpentities.observation import Observation
+from lpentities.organization import Organization
+from lpentities.slice import Slice
+from lpentities.user import User
 from lpentities.value import Value
 from lpentities.year_interval import YearInterval
-from es.weso.worldbank.rest.rest_client import RestClient
-from lpentities.slice import Slice
-from lpentities.instant import Instant
+from reconciler.country_reconciler import CountryReconciler
 from model2xml.model2xml import ModelToXMLTransformer
-
 from requests.exceptions import ConnectionError
+
+from es.weso.worldbank.rest.rest_client import RestClient
 
 
 class Parser(object):
@@ -37,6 +37,8 @@ class Parser(object):
         self.config = ConfigParser.ConfigParser()
         self.config.read('../configuration/api_access.ini')
 
+        self._reconciler = CountryReconciler()
+
         self._org_id = self.config.get("TRANSLATOR", "org_id")
         self._obs_int = int(self.config.get("TRANSLATOR", "obs_int"))
         self._sli_int = int(self.config.get("TRANSLATOR", "sli_int"))
@@ -48,80 +50,145 @@ class Parser(object):
         self.countries_url = self.config.get('URLs', 'country_list')
         self.observations_url = self.config.get('URLs', 'indicator_pattern')
         self.data_sources = dict(self.config.items('data_sources'))
-        self.user = None
-
+        
+        self._organization = self._build_default_organization()
+        self._user = self._build_default_user()
+        self._license = self._build_default_license()
+        
     def model_to_xml(self):
-        for datasource in self.user.organization.data_sources:
+        for datasource in self._user.organization.data_sources:
             for dataset in datasource.datasets:
-                transformer = ModelToXMLTransformer(dataset, "Request", self.user)
+                transformer = ModelToXMLTransformer(dataset, "Request", self._user)
                 transformer.run()
-
 
     def extract_countries(self):
         response = RestClient.get(self.countries_url, {"format": "json"})
         countries = response[1]
         for country in countries:
-            self.countries.append(Country(country['name'],
-                                          None,
-                                          country['iso2Code'],
-                                          country['id']))
-
+            try:
+                self.countries.append(self._reconciler.get_country_by_iso2(country['iso2Code']))
+            except:
+                try:
+                    self.countries.append(self._reconciler.get_country_by_en_name(country['name']))
+                except:
+                    self.logger.warning("No country matches found for iso code" + country['iso2Code'])
+                
+    def _build_default_organization(self):
+        return Organization(chain_for_id=self._org_id,
+                            name=self.config.get("ORGANIZATION", "name"),
+                            url=self.config.get("ORGANIZATION", "url"),
+                            url_logo=self.config.get("ORGANIZATION", "url_logo"),
+                            description=self.config.get("ORGANIZATION", "description"))
+                                  
+    def _build_default_user(self):
+        return User(user_login="worldbank_importer",
+                         organization=self._organization)
+        
+    def _build_default_license(self):
+        return License(name=self.config.get("LICENSE", "name"),
+                       description=self.config.get("LICENSE", "description"),
+                       republish=self.config.get("LICENSE", "republish"),
+                       url=self.config.get("LICENSE", "url"))
+        
+    def _build_data_source(self, data_source_name):
+        data_source = DataSource(chain_for_id=self._org_id,
+                                 int_for_id=self._sou_int,
+                                 name=data_source_name,
+                                 organization=self._organization)
+        self._sou_int += 1 # Updating data sources id value
+        return data_source
+        
+    def _build_data_set(self, data_source):
+        frequency = Dataset.YEARLY
+        dataset = Dataset(chain_for_id=self._org_id,
+                              int_for_id=self._dat_int,
+                              frequency=frequency,
+                              license_type=self._license,
+                              source=data_source)
+        self._dat_int += 1  # Updating dataset int id value
+        return dataset
+    
+    def _build_indicator(self, indicator_element, dataset, measurement_unit):
+        indicator = Indicator(chain_for_id=self._org_id,
+                              int_for_id=self._ind_int,
+                              name_en=indicator_element,
+                              name_es="Desconocido",  # TODO: translate
+                              name_fr="Inconnu",  # TODO: translate
+                              description_en=indicator_element,  # TODO: right now, same as name
+                              description_es="Desconocido",  # TODO: translate
+                              description_fr="Inconnu",  # TODO: translate
+                              dataset=dataset,
+                              measurement_unit=measurement_unit,
+                              preferable_tendency=self._get_preferable_tendency_of_indicator(indicator_element),
+                              topic=Indicator.TOPIC_TEMPORAL)  # TODO: temporal value
+        self._ind_int += 1  # Updating indicator id int value
+        
+        return indicator
+    
+    def _build_slice(self, country, dataset, indicator):
+        slice_object = Slice(chain_for_id=self._org_id,
+                      int_for_id=self._sli_int,
+                      dimension=country,
+                      dataset=dataset,
+                      indicator=indicator)
+        self._sli_int += 1  # Updating int id slice value
+        
+        return slice_object
+    
+    def _build_value(self, indicator, country, date, value_element):
+        value_object = Value(value_element,
+                             Value.FLOAT,
+                             Value.AVAILABLE)
+        if value_object.value is None:
+            value_object = Value(None,
+                                 None,
+                                 Value.MISSING)
+            self.logger.warning('Missing value for ' + indicator.name_en + ', ' + country.name +
+                                                        ', ' + date)
+            
+        return value_object
+    
+    def _build_observation(self, indicator, dataset, country, value, date):
+        value_object = self._build_value(indicator,
+                                         country, 
+                                         date,
+                                         value)
+                                
+        time = YearInterval(date)
+        observation = Observation(chain_for_id=self._org_id,
+                                  int_for_id=self._obs_int,
+                                  ref_time=time,
+                                  issued=Instant(datetime.now()),
+                                  computation=Computation(Computation.RAW),
+                                  value=value_object,
+                                  indicator=indicator,
+                                  dataset=dataset)
+        self._obs_int += 1  # Updating obs int value
+        
+        return observation
+    
     def extract_observations(self, historic, requested_year):
-        organization = Organization(chain_for_id=self._org_id,
-                                    name="World Bank",
-                                    url='http://www.worldbank.org/',
-                                    is_part_of=None)
-        self.user = User(user_login="worldbank_importer",
-                         organization=organization)
         for data_source_name in self.data_sources:
             indicators_section = self.config.get('data_sources', data_source_name)
             requested_indicators = dict(self.config.items(indicators_section))
-            data_source = DataSource(chain_for_id=self._org_id,
-                                     int_for_id=self._sou_int,
-                                     name=data_source_name,
-                                     organization=organization)
-            self._sou_int += 1  # Updating datasource int id value
-            organization.add_data_source(data_source)
-            frequency = Dataset.YEARLY
-            license_object = License('World Bank License',
-                                     'World Bank License',
-                                     True,
-                                     'www.worldbank.org/terms-of-use-datasets')
-            dataset = Dataset(chain_for_id=self._org_id,
-                              int_for_id=self._dat_int,
-                              frequency=frequency,
-                              license_type=license_object,
-                              source=data_source)
-            self._dat_int += 1  # Updating dataset int id value
+            
+            data_source = self._build_data_source(data_source_name)
+            self._organization.add_data_source(data_source)
+            dataset = self._build_data_set(data_source)
             data_source.add_dataset(dataset)
+            
             #print data_source_name
             for indicator_element in requested_indicators:
                 start = indicator_element.index('(') + 1
                 end = indicator_element.index(')')
                 measurement_unit = MeasurementUnit(indicator_element[start:end])
-                indicator = Indicator(chain_for_id=self._org_id,
-                                      int_for_id=self._ind_int,
-                                      name_en=indicator_element,
-                                      name_es="Desconocido",  # TODO: translate
-                                      name_fr="Inconnu",  # TODO: translate
-                                      description_en=indicator_element,  # TODO: right now, same as name
-                                      description_es="Desconocido",  # TODO: translate
-                                      description_fr="Inconnu",  # TODO: translate
-                                      dataset=dataset,
-                                      measurement_unit=measurement_unit,
-                                      preferable_tendency=self._get_preferable_tendency_of_indicator(indicator_element),
-                                      topic=Indicator.TOPIC_TEMPORAL)  # TODO: temporal value
-                self._ind_int += 1  # Updating indicator id int value
+                indicator = self._build_indicator(indicator_element, dataset, measurement_unit)
+                
                 print '\t' + indicator.name_en  + "--------------" + indicator.preferable_tendency + "-----------"
                 web_indiccator_id = self.config.get(indicators_section, indicator_element)
                 for country in self.countries:
-                    slice_object = Slice(chain_for_id=self._org_id,
-                                         int_for_id=self._sli_int,
-                                         dimension=country,
-                                         dataset=dataset,
-                                         indicator=indicator)
+                    slice_object = self._build_slice(country, dataset, indicator)
                     dataset.add_slice(slice_object)  # TESTING EFFECT
-                    self._sli_int += 1  # Updating int id slice value
                     #print '\t\t' + slice_object.slice_id + '\t' + slice_object.dimension.get_dimension_string()
                     uri = self.observations_url.replace('{ISO2CODE}', country.iso2)
                     uri = uri.replace('{INDICATOR.CODE}', web_indiccator_id)
@@ -130,25 +197,13 @@ class Parser(object):
                         observations = response[1]
                         if observations is not None:
                             for observation_element in observations:
-                                value_object = Value(observation_element['value'],
-                                                     "float",
-                                                     Value.AVAILABLE)
-                                if value_object.value is None:
-                                    value_object = Value(None,
-                                                         None,
-                                                         Value.MISSING)
-                                    self.logger.warning('Missing value for ' + indicator.name_en + ', ' + country.name +
-                                                        ', ' + observation_element['date'])
-                                time = YearInterval(observation_element['date'])
-                                observation = Observation(chain_for_id=self._org_id,
-                                                          int_for_id=self._obs_int,
-                                                          ref_time=time,
-                                                          issued=Instant(datetime.now()),
-                                                          computation=Computation(Computation.RAW),
-                                                          value=value_object,
-                                                          indicator=indicator,
-                                                          dataset=dataset)
-                                self._obs_int += 1  # Updating obs int value
+                                #print observation_element
+                                observation = self._build_observation(indicator, 
+                                                                      dataset, 
+                                                                      country, 
+                                                                      observation_element['value'], 
+                                                                      observation_element['date'])
+                                
                                 if historic or observation_element['date'] == requested_year:
                                     country.add_observation(observation)
                                     dataset.add_observation(observation)
