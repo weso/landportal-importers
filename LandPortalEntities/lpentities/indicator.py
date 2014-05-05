bb560b65535cc5a8dccf2a36697aeb24cdf05978
DaniFdezAlvarez <danifdezalvarez@gmail.com>
2014-05-05 14:09:11 +0200

    Add new MeasurementUnit attributes

--- LandPortalEntities/lpentities/indicator.py
+++ LandPortalEntities/lpentities/indicator.py
@@ -6,6 +6,10 @@ Created on 19/12/2013
 
 
 class Indicator(object):
+    """
+    classdocs
+
+    """
 
     #Simulated Enum Values
     INCREASE = "increase"
@@ -15,17 +19,15 @@ class Indicator(object):
     #Possible topics
     _topics_set = ['CLIMATE_CHANGE', 'GEOGRAPH_SOCIO', 'LAND_GENDER', 'LAND_TENURE', 'FSECURITY_HUNGER', 'TEMP_TOPIC']
 
-    '''
-    classdocs
-    '''
+
 
     def __init__(self, chain_for_id, int_for_id, name_en=None, name_es=None,
                  name_fr=None, description_en=None, description_es=None,
                  description_fr=None, dataset=None, measurement_unit=None,
                  topic=None, preferable_tendency=None):
-        '''
+        """
         Constructor
-        '''
+        """
         self.name_en = name_en
         self.name_es = name_es
         self.name_fr = name_fr
@@ -46,10 +48,10 @@ class Indicator(object):
     def __set_topic(self, topic):
         if topic.upper() in self._topics_set:
             self.topic = topic
-        else :
+        else:
             raise ValueError("Provided topic not in the specified list")
         
-    topic = property(fget = __get_topic, fset = __set_topic, doc="Topic of the indicator")
+    topic = property(fget=__get_topic, fset=__set_topic, doc="Topic of the indicator")
     
     @staticmethod
     def _generate_id(chain_for_id, int_for_id):
--- LandPortalEntities/lpentities/measurement_unit.py
+++ LandPortalEntities/lpentities/measurement_unit.py
@@ -9,9 +9,34 @@ class MeasurementUnit(object):
     classdocs
     '''
 
+    RANK = "rank"
+    INDEX = "index"
+    UNITS = "units"
+    SQ_KM = "sq. km"
+    PERCENTAGE = "%"
 
-    def __init__(self, name = None):
+
+    #Enum possible convert_to values
+
+
+    def __init__(self, name=None, convert_to=None, factor=1):
         '''
         Constructor
         '''
         self.name = name
+        self.convert_to = convert_to
+        self.factor = factor
+
+    def __get_convert_to(self):
+        return self.convert_to
+
+    def __set_convert_to(self, value):
+        if value not in [self.RANK, self.INDEX, self.UNITS, self.SQ_KM, self.PERCENTAGE]:
+            raise ValueError("Invalid provided convert_to value: {0}".format(value))
+        self.convert_to = value
+
+    convert_to = property(fget=__get_convert_to,
+                          fset=__set_convert_to,
+                          doc="The MeasurementUnit is convertible to this value")
+
+
